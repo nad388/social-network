2a1bfe63c15c8bc4fa80443a041b831f23f433fc
nad388 <nad388@yandex.ru>
2022-08-09 14:02:41 +0300

    refactoring in OOP style; created store

--- src/index.js
+++ src/index.js
@@ -1,15 +1,9 @@
-import state, { subscribe } from './redux/state';
+import store from './redux/state';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
-import {
-  addMessage,
-  addPost,
-  updateNewMessageText,
-  updateNewPostText,
-} from './redux/state';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 const rerenderEntireTree = (state) => {
@@ -18,15 +12,15 @@ const rerenderEntireTree = (state) => {
       <BrowserRouter>
         <App
           state={state}
-          addPost={addPost}
-          addMessage={addMessage}
-          updateNewPostText={updateNewPostText}
-          updateNewMessageText={updateNewMessageText}
+          addPost={store.addPost.bind(store)}
+          addMessage={store.addMessage.bind(store)}
+          updateNewPostText={store.updateNewPostText.bind(store)}
+          updateNewMessageText={store.updateNewMessageText.bind(store)}
         />
       </BrowserRouter>
     </React.StrictMode>
   );
 };
 
-rerenderEntireTree(state);
-subscribe(rerenderEntireTree);
+rerenderEntireTree(store.getState());
+store.subscribe(rerenderEntireTree);
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,73 +1,71 @@
-let rerenderEntireTree = () => {
-  console.log('state changed');
-};
+let store = {
+  _state: {
+    profilePage: {
+      postsData: [
+        { id: 1, message: 'I am dead!?', likesCount: 5 },
+        { id: 2, message: 'Who killed Laura Palmer?', likesCount: 25 },
+      ],
+      newPostText: 'Enter post...',
+    },
+    dialogsPage: {
+      dialogsData: [
+        { id: 1, name: 'Laura Palmer' },
+        { id: 2, name: 'Dale Cooper' },
+        { id: 3, name: 'Liland Palmer' },
+        { id: 4, name: 'Doppelganger' },
+        { id: 5, name: 'Donna' },
+        { id: 6, name: 'Lady with log' },
+      ],
 
-let state = {
-  profilePage: {
-    postsData: [
-      { id: 1, message: 'I am dead!?', likesCount: 5 },
-      { id: 2, message: 'Who killed Laura Palmer?', likesCount: 25 },
-    ],
-    newPostText: 'Enter post...',
+      messagesData: [
+        { id: 1, message: 'I am dead!?' },
+        { id: 2, message: 'Who killed Laura Palmer?' },
+        { id: 3, message: 'I killed my daughter!' },
+        { id: 4, message: 'My name is Cooper.' },
+        { id: 5, message: 'Where I am?' },
+        { id: 6, message: 'My log always tells the truth.' },
+      ],
+      newMessageText: 'Enter your message...',
+    },
+    sidebar: [],
   },
-  dialogsPage: {
-    dialogsData: [
-      { id: 1, name: 'Laura Palmer' },
-      { id: 2, name: 'Dale Cooper' },
-      { id: 3, name: 'Liland Palmer' },
-      { id: 4, name: 'Doppelganger' },
-      { id: 5, name: 'Donna' },
-      { id: 6, name: 'Lady with log' },
-    ],
-
-    messagesData: [
-      { id: 1, message: 'I am dead!?' },
-      { id: 2, message: 'Who killed Laura Palmer?' },
-      { id: 3, message: 'I killed my daughter!' },
-      { id: 4, message: 'My name is Cooper.' },
-      { id: 5, message: 'Where I am?' },
-      { id: 6, message: 'My log always tells the truth.' },
-    ],
-    newMessageText: 'Enter your message...',
+  getState() {
+    return this._state;
   },
-  sidebar: [],
+  _callSubscriber() {
+    console.log('state changed');
+  },
+  addPost() {
+    let newPost = {
+      id: 3,
+      message: this._state.profilePage.newPostText,
+      likesCount: 0,
+    };
+    this._state.profilePage.postsData.push(newPost);
+    this._state.profilePage.newPostText = ''; // очищаем поле в state
+    this._callSubscriber(this._state);
+  },
+  addMessage() {
+    let newMessage = {
+      id: 7,
+      message: this._state.dialogsPage.newMessageText,
+    };
+    this._state.dialogsPage.messagesData.push(newMessage);
+    this._state.dialogsPage.newMessageText = '';
+    this._callSubscriber(this._state);
+  },
+  updateNewPostText(newText) {
+    this._state.profilePage.newPostText = newText;
+    this._callSubscriber(this._state);
+  },
+  updateNewMessageText(newTextMessage) {
+    this._state.dialogsPage.newMessageText = newTextMessage;
+    this._callSubscriber(this._state);
+  },
+  subscribe(observer) {
+    this._callSubscriber = observer;
+  },
 };
 
-window.state = state;
-
-export const addPost = () => {
-  let newPost = {
-    id: 3,
-    message: state.profilePage.newPostText,
-    likesCount: 0,
-  };
-  state.profilePage.postsData.push(newPost);
-  state.profilePage.newPostText = ''; // очищаем поле в state
-  rerenderEntireTree(state);
-};
-
-export const addMessage = () => {
-  let newMessage = {
-    id: 7,
-    message: state.dialogsPage.newMessageText,
-  };
-  state.dialogsPage.messagesData.push(newMessage);
-  state.dialogsPage.newMessageText = '';
-  rerenderEntireTree(state);
-};
-
-export const updateNewPostText = (newText) => {
-  state.profilePage.newPostText = newText;
-  rerenderEntireTree(state);
-};
-
-export const updateNewMessageText = (newTextMessage) => {
-  state.dialogsPage.newMessageText = newTextMessage;
-  rerenderEntireTree(state);
-};
-
-export const subscribe = (observer) => {
-  rerenderEntireTree = observer;
-};
-
-export default state;
+export default store;
+window.store = store;
