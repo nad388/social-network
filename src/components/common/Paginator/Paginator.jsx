f86ad04caf2adcf7d7321729ac817440336917ca
nad388 <nad388@yandex.ru>
2022-09-10 16:58:49 +0300

    changed paginator

--- src/components/common/Paginator/Paginator.jsx
+++ src/components/common/Paginator/Paginator.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './Paginator.module.css';
 
 const Paginator = ({
@@ -6,33 +6,59 @@ const Paginator = ({
   pageSize,
   currentPage,
   onPageChanged,
+  portionSize = 10,
 }) => {
-  let pagesCount = Math.ceil(totalUsersCount / pageSize);
+  const pagesCount = Math.ceil(totalUsersCount / pageSize);
 
-  let pages = [];
-  for (
-    let i = Math.max(currentPage - 5, 1);
-    i <= Math.max(1, Math.min(currentPage + 5, pagesCount));
-    i++
-  ) {
+  const pages = [];
+  for (let i = 1; i <= pagesCount; i++) {
     pages.push(i);
   }
 
+  const portionCount = Math.ceil(pagesCount / portionSize);
+  const [portionNumber, setPortionNumber] = useState(1);
+  const leftPortionPageNumber = (portionNumber - 1) * portionSize + 1;
+  const rightPortionPageNumber = portionNumber * portionSize;
+
   return (
     <div className={styles.btnPages}>
-      {pages.map((p) => {
-        return (
-          <span
-            className={currentPage === p ? styles.selectedPage : ''}
-            onClick={(e) => {
-              onPageChanged(p);
-            }}
-            key={p}
-          >
-            {p}
-          </span>
-        );
-      })}
+      {portionNumber > 1 && (
+        <button
+          className={styles.btn}
+          onClick={() => {
+            setPortionNumber(portionNumber - 1);
+          }}
+        >
+          PREV
+        </button>
+      )}
+      {pages
+        .filter(
+          (p) => p >= leftPortionPageNumber && p <= rightPortionPageNumber
+        )
+        .map((p) => {
+          return (
+            <span
+              className={currentPage === p ? styles.selectedPage : ''}
+              onClick={(e) => {
+                onPageChanged(p);
+              }}
+              key={p}
+            >
+              {p}
+            </span>
+          );
+        })}
+      {portionCount > portionNumber && (
+        <button
+          className={styles.btn}
+          onClick={() => {
+            setPortionNumber(portionNumber + 1);
+          }}
+        >
+          NEXT
+        </button>
+      )}
     </div>
   );
 };
